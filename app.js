300b5add6bd59a54c5dcd27c411d2490117102e3
Sahas Prajapati <b10prajapati@gmail.com>
2020-11-13 16:02:48 +0545

    Initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const tf = require("@tensorflow/tfjs-node");
+const port = 3000;
+
+app.use(cors());
+app.use(express.static("public"));
+
+app.get("/hello", async (req, res) => {
+  const model = await tf.loadGraphModel("file://multvae/model.json");
+
+  let zeros = (w, h, v = 0) =>
+    Array.from(new Array(h), (_) => Array(w).fill(v));
+
+  const arr = zeros(62000, 1, 0);
+  console.log(arr);
+  const tensor = tf.tensor(arr);
+  const result = await model.predict(tensor);
+  console.log(result);
+  console.log("hello");
+  res.send("hello world!");
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
